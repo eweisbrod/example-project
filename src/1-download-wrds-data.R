99f9dae2786a08dbcf1dd143437d0e3167fcbe1b
Weisbrod <e679w418@home.ku.edu>
2025-01-29 13:45:19 -0600

    fixed a few typos and improved some syntax.

--- src/1-download-wrds-data.R
+++ src/1-download-wrds-data.R
@@ -37,8 +37,8 @@ wrds  # checking if connection exists
 # Just an example to play with the Postgres server
 
 # List all of the tables in Compustat (comp)
-wrds %>%
-  DBI::dbListObjects(DBI::Id(schema = 'ibes')) |> 
+wrds |>
+  DBI::dbListObjects(DBI::Id(schema = 'comp')) |> 
   dplyr::pull(table) |> 
   purrr::map(~slot(.x, 'name'))  |> 
   dplyr::bind_rows()  |>  
@@ -62,8 +62,10 @@ tictoc::tic()
 raw_funda <-
   comp.funda |> 
   #Apply standard Compustat filters
-  filter(indfmt=='INDL', datafmt=='STD', popsrc=='D' ,consol=='C') %>%
-  #Select the variables we want to dowload
+  filter(indfmt=='INDL', datafmt=='STD', popsrc=='D' ,consol=='C') |>
+  #Select the variables we want to download
+  #the pattern for inline renaming is 
+  #new_name = old_name
   select(conm, gvkey, datadate, fyear, fyr, cstat_cusip=cusip, #inline renaming
          cik, cstat_ticker= tic, sich, ib, spi, at, xrd, ceq, sale,
          csho, prcc_f
@@ -71,7 +73,7 @@ raw_funda <-
   #Merge with the Compustat Company file for header SIC code and GICs code
   inner_join(select(comp.company, gvkey, sic, fic, gind), by="gvkey") |> 
   #Use historical sic [sich] when available. Otherwise use header sic [sic]
-  mutate(sic4 = case_when( is.null(sich) ~ as.numeric(sic), TRUE ~ sich)) |> 
+  mutate(sic4 = coalesce(sich, as.numeric(sic))) |> 
   #Calculate two digit sic code
   mutate(sic2 = floor(sic4/100)) |> 
   #Delete financial and utility industries
@@ -88,7 +90,7 @@ raw_funda <-
     # Some papers use June of each year and assume a 3 month reporting lag.
     # Effectively this is coded as aligning datadate as of March each year.
     # See, for example, Hou, Van Dijk, and Zhang (2012 JAE) figure 1
-    # This examine also demonstrates injecting sql into dplyr code
+    # This example also demonstrates injecting sql into dplyr code
     calyear = if_else( fyr > 3,
                        sql("extract(year from datadate)")+1,
                        sql("extract(year from datadate)")),
@@ -96,7 +98,7 @@ raw_funda <-
     mve = csho * prcc_f,
     # define earnings (e) as earnings before special items
     e= ib-spi,
-  ) %>%
+  ) |>
   # filter to fiscal years after 1955, not much in Compustat before that 
   filter(1967 < fyear) |> 
   # filter to US companies
@@ -112,12 +114,15 @@ raw_funda <-
 #stop the tictoc timer
 tictoc::toc()
 
+
 # Save the data to disk --------------------------------------------------------
 
 # saving to Stata is convenient for working with coauthors
 # glue package allows for dynamic file paths 
 # then each coauthor can specify their own local data folder
+tic()
 write_dta(raw_funda,glue("{data_path}/raw-data-R.dta")) 
+toc()
 #looks like about 162 MB on my machine
 
 # if the data will stay in R or another advanced/modern language like Python
@@ -128,7 +133,9 @@ write_dta(raw_funda,glue("{data_path}/raw-data-R.dta"))
 # default to a high level of gzip compression to save space
 # therefore, the write_parquet function is using the function defined in the 
 # utils script
+tic()
 write_parquet(raw_funda,glue("{data_path}/raw-data-R.parquet"))
+toc()
 # the parquet operations are faster and the file is only 32MB on my machine
 
 
